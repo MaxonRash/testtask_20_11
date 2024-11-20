de0cabc5e72d6cfad659bbbc2785d3cefbfa8e51
MaxonRash <rashnikov.m@yandex.com>
2024-11-20 13:35:29 +0300

    Initial commit

---
+++ src/main/java/com/task/testtask_20_11/Testtask2011Application.java
@@ -0,0 +1,13 @@
+package com.task.testtask_20_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testtask2011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testtask2011Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/testtask_20_11/Testtask2011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.testtask_20_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testtask2011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
